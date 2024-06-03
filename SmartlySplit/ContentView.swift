b678efc3854a187029bcabfc55d34b9f8aa32b6f
vikki1310 <156143195+vikki1310@users.noreply.github.com>
2024-06-03 16:48:16 +1000

    picker, forEach

--- SmartlySplit/ContentView.swift
+++ SmartlySplit/ContentView.swift
@@ -3,16 +3,26 @@ import SwiftUI
 struct ContentView: View {
     @State var tapCount = 0
     @State var name = ""
+    let students = ["abc", "cdf", "efg"]
+    @State private var selectStudent = ""
     var body: some View {
         NavigationStack{
             Form {
                 Section{
-                    TextField("Enter something here", text: $name)
-                    Text("Hello !!")
+                    TextField("Enter something here", text: $name) // two way binding
+                    Text("Hello !!\(name)")
                     Text("Hello!!")
                 }
                 Button("Tap: \(tapCount)"){
                     tapCount += 1
+                }
+                
+                Section{
+                    Picker("Select the student", selection: $selectStudent){
+                        ForEach(students, id:\.self){
+                            Text($0)
+                        }
+                    }
                 }
             }
             .font(.system(size:15, design: .default))
